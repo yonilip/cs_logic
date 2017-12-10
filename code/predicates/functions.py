7a936726f577e173e2733629b31f277b49635a63
Ayal Moses <ayal@lightricks.com>
2017-12-10 18:04:23 +0200

    Ex8 initial commit.

---
+++ code/predicates/functions.py
@@ -0,0 +1,111 @@
+""" (c) This file is part of the course
+    Mathematical Logic through Programming
+    by Gonczarowski and Nisan.
+    File name: code/predicates/functions.py """
+
+from predicates.syntax import *
+from predicates.semantics import *
+from predicates.util import *
+
+def replace_functions_with_relations_in_model(model):
+    """ Return a new model obtained from the given model by replacing every
+        function meaning with the corresponding relation meaning (i.e.,
+        (x1,...,xn) is in the meaning if and only if x1 is the output of the
+        function meaning for the arguments x2,...,xn), assigned to a relation
+        with the same name as the function, except that the first letter is
+        capitalized """
+    assert type(model) is Model
+    # Task 8.2
+
+def replace_relations_with_functions_in_model(model, original_functions):
+    """ Return a new model original_model with function names
+        original_functions such that:
+        model == replace_functions_with_relations_in_model(original_model)
+        or None if no such original_model exists """
+    assert type(model) is Model
+    # Task 8.3
+
+def compile_term(term):
+    """ Return a list of steps that result from compiling the given term,
+        whose root is a function invocation (possibly with nested function
+        invocations down the term tree). Each of the returned steps is a
+        Formula of the form y=f(x1,...,xk), where the y is a new variable name
+        obtained by calling next(fresh_variable_name_generator) (you may assume
+        that such variables do not occur in the given term), f is a
+        single function invocation, and each of the xi is either a constant or
+        a variable. The steps should be ordered left-to-right between the
+        arguments, and within each argument, the computation of a variable
+        value should precede its usage. The left-hand-side variable of the last
+        step should evaluate to the value of the given term """
+    assert type(term) is Term and is_function(term.root)
+    # Task 8.4
+
+def replace_functions_with_relations_in_formula(formula):
+    """ Return a function-free analog of the given formula. Every k-ary
+        function that is used in the given formula should be replaced with a
+        k+1-ary relation with the same name except that the first letter is
+        capitalized (e.g., the function plus(x,y) should be replaced with the
+        relation Plus(z,x,y) that stands for z=plus(x,y)). (You may assume
+        that such relation names do not occur in the given formula, which
+        furthermore does not contain any variables names starting with z.) The
+        returned formula need only be equivalent to the given formula for
+        models where each new relations encodes a function, i.e., is guaranteed
+        to have single possible value for the first relation argument for every
+        k-tuple of the other arguments """
+    assert type(formula) is Formula
+    # Task 8.5
+
+def replace_functions_with_relations_in_formulae(formulae):
+    """ Return a list of function-free formulae (as strings) that is equivalent
+        to the given formulae list (also of strings) that may contain function
+        invocations. This equivalence is in the following sense:
+        for every model of the given formulae,
+        replace_functions_with_relations_in_model(model) should be a model
+        of the returned formulae, and for every model of the returned formulae,
+        replace_relations_with_functions_in_model(model) should be a model
+        of the given formulae. Every k-ary function that is used in the given
+        formulae should be replaced with a k+1-ary relation with the same name
+        except that the first letter is capitalized (e.g., the function
+        plus(x,y) should be replaced with the relation Plus(z,x,y) that stands
+        for z=plus(x,y)). (You may assume that such relation names do not occur
+        in the given formulae, which furthermore does not contain any variables
+        names starting with z.) The returned list should have one formula for
+        each formula in the given list, as well as one additional formula for
+        every relation that replaces a function name from the given list """
+    for formula in formulae:
+        assert type(formula) is str
+    # task 8.6
+        
+def replace_equality_with_SAME(formulae):
+    """ Return a list of equality-free formulae (as strings) that is equivalent
+        to the given formulae list (also of strings) that may contain the
+        equality symbol. Every occurrence of equality should be replaced with a
+        matching instantiation of the relation SAME, which you may assume
+        does not occur in the given formulae. You may assume that the given
+        formulae do not contain any function invocations. The returned list
+        should have one formula for each formula in the given list, as well as
+        additional formulae that ensure that SAME is reflexive, symmetric,
+        transitive, and respected by all relations in the given formulae """
+    for formula in formulae:
+        assert type(formula) is str
+    # Task 8.7
+        
+def add_SAME_as_equality(model):
+    """ Return a new model obtained from the given model by adding the relation
+        SAME that behaves like equality """
+    assert type(model) is Model
+    # Task 8.8
+    
+def make_equality_as_SAME(model):
+    """ Return a new model where equality is made to coincide with the
+        reflexive, symmetric, transitive, and respected by all relations,
+        relation SAME in the the given model. The requirement is that for every
+        model and every list formulae_list, if we take
+        new_formulae=replace_equality_with_SAME(formulae) and
+        new_model=make_equality_as_SAME(model) then model is a valid model
+        of new_formulae if and only if new_model is a valid model of formulae.
+        The universe of the returned model should correspond to the equivalence
+        classes of the SAME relation in the given model. You may assume that
+        there are no function meanings in the given model """
+    assert type(model) is Model
+    # Task 8.9
---
+++ code/predicates/functions_test.py
@@ -0,0 +1,251 @@
+""" (c) This file is part of the course
+    Mathematical Logic through Programming
+    by Gonczarowski and Nisan.
+    File name: code/predicates/functions_test.py """
+
+from predicates.syntax import *
+from predicates.semantics import *
+from predicates.functions import *
+
+def test_replace_functions_with_relations_in_model(debug):
+    model = Model(
+        {'a', 'b'},
+        {'a': 'a',
+         'GT': {('b','a')}, 'f': {('a',):'b', ('b',):'b'},
+         'gg': {('a','a'):'b', ('a','b'):'a', ('b','a'):'a', ('b','b'):'b'}})
+    if debug:
+        print('Replacing functions with relations in model', model, '...')
+    new_model = replace_functions_with_relations_in_model(model)
+    if debug:
+        print('... got', new_model)
+    assert new_model.universe == model.universe
+    assert new_model.meaning.keys() == {'a', 'F', 'Gg', 'GT'}
+    assert new_model.meaning['a'] == 'a'
+    assert new_model.meaning['F'] == {('b','a'), ('b','b')}
+    assert new_model.meaning['Gg'] == {('b','a','a'), ('a','a','b'),
+                                       ('a','b','a'), ('b','b','b')}
+    assert new_model.meaning['GT'] == {('b','a')}
+    
+def test_replace_relations_with_functions_in_model(debug):
+    model = Model(
+        {'a', 'b'},
+        {'a': 'a',
+         'GT': {('b','a')}, 'F' :{('b','a'), ('b','b')},
+         'GG': {('b','a','a'), ('a','a','b'), ('a','b','a'), ('b','b','b')}})
+
+    if debug:
+        print('Replacing relations with functions in model', model, '...')
+    new_model = replace_relations_with_functions_in_model(model, {'f', 'gG'})
+    if debug:
+        print('... got', new_model)
+    assert new_model.universe == model.universe
+    assert new_model.meaning.keys() == {'a', 'f', 'gG', 'GT'}, new_model.meaning.keys()
+    assert new_model.meaning['a'] == 'a'
+    assert new_model.meaning['f'] == {('a',):'b', ('b',):'b'}
+    assert new_model.meaning['gG'] == {('a','a'):'b', ('a','b'):'a',
+                                       ('b','a'):'a', ('b','b'):'b'}
+    assert new_model.meaning['GT'] == {('b','a')}
+
+    if debug:
+        print('Replacing relations F with functions in model', model, '...')
+    new_model = replace_relations_with_functions_in_model(model, {'f'})
+    if debug:
+        print('... got', new_model)
+    assert new_model.universe == model.universe
+    assert new_model.meaning.keys() == {'a', 'f', 'GG', 'GT'}
+    assert new_model.meaning['a'] == 'a'
+    assert new_model.meaning['f'] == {('a',):'b', ('b',):'b'}
+    assert new_model.meaning['GG'] == {('b','a','a'), ('a','a','b'),
+                                       ('a','b','a'), ('b','b','b')}
+    assert new_model.meaning['GT'] == {('b','a')}
+
+    # Test faulty models
+    model = Model(
+        {'a', 'b'},
+        {'a': 'a',
+         'GT': {('b','a')}, 'F' :{('b','a'), ('b','b'), ('a','b')},
+         'GG': {('b','a','a'), ('a','a','b'), ('a','b','a'), ('b','b','b')}})
+    if debug:
+        print('Replacing relations with functions in model', model, '...')
+    new_model = replace_relations_with_functions_in_model(model, {'f', 'gG'})
+    assert new_model == None
+
+    model = Model(
+        {'a', 'b'},
+        {'a': 'a',
+         'GT': {('b','a')}, 'F' :{('b','a'), ('b','b')},
+         'GG': {('b','a','a'), ('a','a','b'), ('b','b','b')}})
+    if debug:
+        print('Replacing relations with functions in model', model, '...')
+    new_model = replace_relations_with_functions_in_model(model, {'f', 'gG'})
+    assert new_model == None
+
+def test_compile_term(debug):
+    for s,expected in [
+            ['f(x,g(0))', ['z1=g(0)', 'z2=f(x,z1)']],
+            ['f(g(x,h(0)),f(f(0,g(y)),h(h(x))))',
+             ['z3=h(0)', 'z4=g(x,z3)', 'z5=g(y)', 'z6=f(0,z5)', 'z7=h(x)',
+              'z8=h(z7)', 'z9=f(z6,z8)', 'z10=f(z4,z9)']],
+            ['f(x,g(0))', ['z11=g(0)', 'z12=f(x,z11)']]]:
+        term = Term.parse(s)
+        if debug:
+            print('Compiling', term, '...')
+        steps = compile_term(term)
+        if debug:
+            print('... got', steps)
+        assert steps == [Formula.parse(e) for e in expected]
+
+def test_replace_functions_with_relations_in_formula(debug):
+    for s,valid_model,invalid_model in [
+        ['b=f(a)',
+         Model({'a', 'b'}, {'a': 'a', 'b': 'b', 'F':{('b','a'), ('b','b')}}),
+         Model({'a', 'b'}, {'a': 'a', 'b': 'b', 'F':{('a','a'), ('b','b')}})],
+        ['GT(f(a),g(b))',
+         Model({'a', 'b'},
+               {'a': 'a', 'b': 'b', 'GT': {('b','a')},
+                'F': {('b','a'), ('b','b')}, 'G': {('b','a'), ('a','b')}}),
+         Model({'a', 'b'},
+               {'a': 'a', 'b': 'b', 'GT': {('b','a')},
+                'F': {('a','a'), ('b','b')}, 'G': {('b','a'), ('a','b')}})],
+        ['Ax[f(f(x))=x]',
+         Model({'a', 'b'},
+               {'a': 'a', 'b': 'b', 'GT': {('b','a')},
+                'F': {('a','a'), ('b','b')}, 'G': {('b','a'), ('a','b')}}),
+         Model({'a', 'b'},
+               {'a': 'a', 'b': 'b', 'GT': {('b','a')},
+                'F': {('b','a'), ('b','b')}, 'G': {('b','a'), ('a','b')}})],
+        ['Ax[(Ey[f(y)=x]->GT(x,a))]',
+         Model({'a', 'b'},
+               {'a': 'a', 'b': 'b', 'GT': {('b','a')},
+                'F': {('b','a'), ('b','b')}, 'G': {('b','a'), ('a','b')}}),
+         Model({'a', 'b'},
+               {'a': 'a', 'b': 'b', 'GT': {('b','a')},
+                'F': {('a','a'), ('b','b')}, 'G': {('b','a'), ('a','b')}})]]:
+        formula=Formula.parse(s)
+        if debug:
+            print('Replacing functions with relations in formula', formula,
+                  '...')
+        new_formula = replace_functions_with_relations_in_formula(formula)
+        if debug:
+            print('... got', formula)
+        for model,validity in [[valid_model,True], [invalid_model,False]]:
+            # Will be tested with the course staff's implementation of
+            # is_model_of
+            is_valid_model = model.is_model_of({str(new_formula)})
+            if debug:
+                print('which', 'is' if is_valid_model else 'is not',
+                      'satisfied by model', model)
+            assert is_valid_model == validity
+
+def test_replace_functions_with_relations_in_formulae(debug):
+    formula = 'GT(f(a),g(b))'
+    if debug:
+        print('Removing functions from singleton', formula, '...')
+    new_formulae = replace_functions_with_relations_in_formulae([formula])
+    if debug:
+        print('... got', new_formulae)
+    new_formulae = set(new_formulae)
+
+    for model,validity in [
+            [Model({'a', 'b'},
+                   {'a': 'a', 'b': 'b', 'GT': {('b','a')},
+                    'F': {('b','a'), ('b','b')}, 'G': {('b','a'), ('a','b')}}),
+             True],
+            [Model({'a', 'b'},
+                   {'a': 'a', 'b': 'b', 'GT': {('b','a')},
+                    'F': {('a','a'), ('b','b')}, 'G': {('b','a'), ('a','b')}}),
+             False],
+            [Model({'a', 'b'},
+                   {'a': 'a', 'b': 'b', 'GT': {('b','a')},
+                    'F': {('a','a')}, 'G': {('b','a'), ('a','b')}}),
+             False],
+            [Model({'a', 'b'},
+                   {'a': 'a', 'b': 'b', 'GT': {('b','a')},
+                    'F': {('a','a'), ('b','b'), ('b','a')},
+                    'G': {('b','a'), ('a','b')}}),
+             False],
+            [Model({'a', 'b'},
+                   {'a': 'a', 'b': 'b', 'GT': {('b','a')},
+                    'F': {('a','a'), ('b','b')},
+                    'G': {('b','a'), ('a','b'), ('b','b')}}),
+             False]]:
+        if debug:
+            print('which',
+                  'is' if model.is_model_of(new_formulae) else 'is not',
+                  'satisfied by model', model)
+        # Will be tested with the course staff's implementation of is_model_of
+        assert model.is_model_of(new_formulae) == validity
+
+def test_replace_equality_with_SAME(debug):
+    formula = 'Ax[Ay[Az[((S(x,y)&S(x,z))->y=z)]]]'
+    if debug:
+        print('Removing equalities from singleton', formula, '...')
+    new_formulae = replace_equality_with_SAME([formula])
+    if debug:
+        print('... got', new_formulae)
+    new_formulae = set(new_formulae)
+    assert 'Ax[Ay[Az[((S(x,y)&S(x,z))->SAME(y,z))]]]' in new_formulae
+
+    for model,validity in [
+            [Model({'0', '1', '2'},
+                   {'S': {},
+                    'SAME': {('0','0'), ('1','1'), ('2','2'), ('0','1')}}),
+             False],
+            [Model({'0', '1', '2'},
+                   {'S': {}, 'SAME': {('0','0'), ('1','1')}}),
+             False],
+            [Model({'0', '1', '2'},
+                   {'S': {}, 'SAME': {('0','0'), ('1','1'), ('2','2')}}),
+             True],
+            [Model({'0','1','2'},
+                   {'S': {('0','1'), ('0','2')},
+                    'SAME': {('0','0'), ('1','1'), ('2','2')}}),
+             False],
+            [Model({'0', '1', '2'},
+                   {'S': {('0','1'),('0','2')},
+                    'SAME': {('0','0'), ('1','1'), ('2','2'), ('1','2'),
+                             ('2','1')}}),
+             True],
+            [Model({'0', '1', '2'},
+                   {'S': {('0','1')},
+                    'SAME': {('0','0'), ('1','1'), ('2','2'), ('1','2'),
+                             ('2','1')}}),
+             False]]:
+        if debug:
+            print('which',
+                  'is' if model.is_model_of(new_formulae) else 'is not',
+                  'satisfied by model', model)
+        # Will be tested with the course staff's implementation of is_model_of
+        assert model.is_model_of(new_formulae) == validity
+  
+def test_add_SAME_as_equality(debug):
+    model = Model({'0', '1', '2'}, {'a': '0', 'Q': {('1')}})
+    if debug:
+        print('Adding SAME to model', model, '...')
+    new_model = add_SAME_as_equality(model)
+    if debug:
+        print('... got', new_model)
+    assert new_model.universe == model.universe
+    assert new_model.meaning.keys() == {'a', 'Q', 'SAME'}
+    assert new_model.meaning['a'] == model.meaning['a']
+    assert new_model.meaning['Q'] == model.meaning['Q']
+    assert new_model.meaning['SAME'] == {('0','0'), ('1','1'), ('2','2')}
+
+def test_make_equality_as_SAME(debug):
+    model = Model({'0', '1', '2'},
+                  {'SAME': {('0','0'), ('1','1'), ('2','2'), ('1','2'),
+                            ('2','1')},
+                   'a': '0', 'b': '1', 'c': '2',
+                   'Q': {('0',), ('1',), ('2',)}})
+    if debug:
+        print('Making equality as SAME in model', model)
+    new_model = make_equality_as_SAME(model)
+    if debug:
+        print('... got', new_model)
+    assert len(new_model.universe)==2
+    assert new_model.meaning.keys() == {'a', 'b', 'c', 'Q'}
+    assert new_model.meaning['b'] == new_model.meaning['c']
+    assert new_model.meaning['a'] != new_model.meaning['b']
+    for x in new_model.universe:
+        assert (x,) in new_model.meaning['Q']
+    assert len(new_model.meaning['Q']) == 2
