1f9c090b6cf72132bffea8f1ee7e190c1e06acdb
HolyMoses <ayal.moses@gmail.com>
2018-01-02 19:09:54 +0200

    temp 10.2

--- code/predicates/prover.py
+++ code/predicates/prover.py
@@ -161,15 +161,33 @@ class Prover:
             number of the (new) line in this proof containing conclusion is
             returned """
         # Task 10.2
-        step1, step2 = line_numbers[0], line_numbers[1]
-        a_b = self.proof.lines[step1].formula
-        b_c = self.proof.lines[step2].formula
-        a_c_str = '(' + str(a_b.first) + "->" + str(b_c.second) + ')'
+        tautology = Formula.parse(conclusion)
+        formula_stack = []
+        for current_step in reversed(line_numbers):
+            current_formula = self.proof.lines[current_step].formula
+            tautology = Formula('->', current_formula, tautology)
+            formula_stack.append(tautology)
 
-        step3 = self.add_tautology('(' + str(a_b) + '->(' + str(b_c) + '->' + a_c_str + '))')
-        step4 = self.add_mp('(' + str(b_c) + '->' + conclusion + ')', step1, step3)
-        step5 = self.add_mp(conclusion, step2, step4)
-        return step5
+
+        current_step = self.add_tautology(tautology)
+
+        for i in range(len(line_numbers)):
+            current_step = self.add_mp(formula_stack[i], line_numbers[i], current_step)
+
+        return current_step
+
+
+            #     step1, step2 = line_numbers[0], line_numbers[1]
+        # a_b = self.proof.lines[step1].formula
+        # b_c = self.proof.lines[step2].formula
+        #
+        # step3 = self.add_tautology('(' + str(a_b) + '->(' + str(b_c) + '->' + conclusion + '))')
+        # step4 = self.add_mp('(' + str(b_c) + '->' + conclusion + ')', step1, step3)
+        # step5 = self.add_mp(conclusion, step2, step4)
+        #
+        # return step5
+
+
 
         # step5 = prover.add_tautology(
         #     '((Greek(x)->Human(x))  ->  ((Human(x)->Mortal(x)) -> (Greek(x)->Mortal(x))))')
